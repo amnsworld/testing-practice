53a35f0755ad2942ef5709139a799a7dad429c92
Amandeep Singh <amandeep100393@gmail.com>
2020-04-06 14:24:18 -0400

    Added Function for add

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,6 +1,12 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
+function sum(num1,num2) {
+  if(num1 == undefined) num1 = 0;
+  if(num2 == undefined) num2 = 0;
+  return num1+num2;
+}
+
 function calc() {
 
 }
